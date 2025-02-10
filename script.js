a538622e135487a8d93e5d405938b515d3df6794
SAFF35 <smartaffiliatee@gmail.com>
2025-02-11 01:40:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -20,33 +20,6 @@ document.getElementById('registrasiForm').addEventListener('submit', function(ev
         referral: finalReferral
     };
 
-
-    // Ambil kode referral dari URL
-  // Jika URL berupa ?referral=HYT001, maka ambil nilai dari parameter 'referral'
-  const urlParams = new URLSearchParams(window.location.search);
-  let referralCode = urlParams.get('referral');
-
-  // Jika tidak ada parameter 'referral' tetapi URL diakhiri dengan kode (misalnya: ?HYT001)
-  if (!referralCode && window.location.search && window.location.search.indexOf('=') === -1) {
-    referralCode = window.location.search.substring(1);
-  }
-  if (referralCode) {
-    document.getElementById('referral').value = referralCode;
-  }
-
-  // Menangani submit form
-  document.getElementById('registrationForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const form = event.target;
-    const formData = new FormData(form);
-
-    // Ubah data form menjadi format URL-encoded
-    const params = new URLSearchParams();
-    for (const pair of formData.entries()) {
-      params.append(pair[0], pair[1]);
-    }
-    
-
     fetch('https://script.google.com/macros/s/AKfycbyxyFIoOi6l4NSuWOK7byxS7zKYLnMoyVrSKnJsmGWAN1v6ymY3crEYBR3R3XhXeRfDAw/exec', {
         method: 'POST',
         mode: 'no-cors',
