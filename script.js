9a769c1def09b60149db10f6028753fd54639c26
SAFF35 <smartaffiliatee@gmail.com>
2025-02-11 02:19:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,9 @@ fetch('https://script.google.com/macros/s/AKfycbyxyFIoOi6l4NSuWOK7byxS7zKYLnMoyV
 .then(response => response.text())
 .then(whatsappUrl => {
     alert('Registrasi Berhasil!');
+    window.open(whatsappUrl, '_blank'); // Buka tautan WhatsApp di tab baru
     window.location.reload();
-    })
+})
 .catch(error => {
     console.error('Error:', error);
 });
